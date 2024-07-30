16b9e7bddf4abdccf2c138592f6b56a97b087b89
Benn1440 <benokafor67@gmail.com>
2024-07-30 01:05:44 +0100

    created backend.tf File

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform_bucket"
-#     dynamodb_table = "terraform-lock-table"
-#     encrypt = true
-#     key = "aws_terraform/terraform.tfstate"
-#     region = "eu-west-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "terraform_bucket"
+    dynamodb_table = "terraform-lock-table"
+    encrypt = true
+    key = "aws_terraform/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
